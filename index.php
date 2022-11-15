03fe3d44194677bd4e32c43ee4b2d20c50edbb9f
Kunwar Abhay Rai <Kunwarabhayrai12345@gmail.com>
2022-11-15 22:19:49 +0530

    updated home page

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<style>
+    body {
+        background-image:  linear-gradient(#dddddd36 , #ffffff00) ,url(images/bg-iitp.jpg) ,url(images/bg-iitp.jpg);
+        background-size: cover;
+        height: 100vh;
+        background-position: center;
+    }
+
+    .contributor{
+        background-color: #ffffff5c;
+    }
+</style>
 
 <?php
     session_start();
     session_destroy();
 ?>
 
-<div style='padding: 17px'>
-<h2>Home Page</h2>
-    CS355: Database Lab Project<br>
-    <strong>Kaushal Raj 2001CS36</strong><br>
-    <strong>Kunwar Abhay Rai 2001CS40</strong><br>
-    <strong>Ashutosh Kumar 2001CS11</strong><br>
-    <strong>Vipul Kumar Gond 2001CS77</strong><br>
+
+<h1 class="text-center mb-4">Home Page</h1>
+<h2 class="text-center mb-4">CS355: Database Lab Project</h2>
+
+<div class="container d-flex justify-content-between mb-4">
+  <div class="border border-4 rounded border-dark p-4 contributor">
+    <div class="font-weight-bold">Kaushal Raj</div>
+    <div class="font-weight-bold">2001CS36</div>
+  </div>
+  <div class="border border-4 rounded border-dark p-4 contributor">
+    <div class="font-weight-bold">Kunwar Abhay Rai </div>
+    <div class="font-weight-bold">2001CS40</div>
+  </div>
+  <div class="border border-4 rounded border-dark p-4 contributor">
+    <div class="font-weight-bold">Ashutosh Kumar</div> 
+    <div class="font-weight-bold">2001CS11</div>
+  </div>
+  <div class="border border-4 rounded border-dark p-4 contributor">
+    <div class="font-weight-bold">Vipul Kumar Gond</div>
+    <div class="font-weight-bold">2001CS77</div>
+  </div>
 </div>
 
-<div class="form-group">
-  <label class="col-md-8 control-label"></label>
-  <div class="col-md-8">
-        <a href="guest_house/home_guest_house.php" class="btn btn-primary">Guest House</a>
-        <a href="market/home_market.php" class="btn btn-primary">Market</a>
-    </div>
+<div class="container text-center">
+    <a href="guest_house/home_guest_house.php" class="btn btn-primary">Guest House</a>
+    <a href="market/home_market.php" class="btn btn-primary">Market</a>
 </div>
